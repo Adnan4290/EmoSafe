5655e70c0e74f39c5488f0ec4878c29e1dafcf8a
H4CK3R <h4ck3r@h4ck3r.h4ck3r>
2023-11-13 01:00:57 +0500

    Added serial reading functionality

--- Drowsiness_Detection.py
+++ Drowsiness_Detection.py
@@ -3,54 +3,92 @@ from imutils import face_utils
 import imutils
 import dlib
 import cv2
+import serial
+import threading
 
 def eye_aspect_ratio(eye):
-	A = distance.euclidean(eye[1], eye[5])
-	B = distance.euclidean(eye[2], eye[4])
-	C = distance.euclidean(eye[0], eye[3])
-	ear = (A + B) / (2.0 * C)
-	return ear
-	
+    A = distance.euclidean(eye[1], eye[5])
+    B = distance.euclidean(eye[2], eye[4])
+    C = distance.euclidean(eye[0], eye[3])
+    ear = (A + B) / (2.0 * C)
+    return ear
+
+def read_serial(port, baudrate):
+    try:
+        ser = serial.Serial(port, baudrate)
+        print(f"Reading from {port} at {baudrate} baudrate. Press Ctrl+C to stop.")
+
+        while True:
+            line = ser.readline().decode("utf-8").rstrip()
+            print(line)
+
+    except serial.SerialException as e:
+        print(f"Error: {e}")
+    except KeyboardInterrupt:
+        print("Serial reading stopped.")
+    finally:
+        if ser.is_open:
+            ser.close()
+
 thresh = 0.25
 frame_check = 20
 detect = dlib.get_frontal_face_detector()
-predict = dlib.shape_predictor("models/shape_predictor_68_face_landmarks.dat")# Dat file is the crux of the code
+predict = dlib.shape_predictor("models/shape_predictor_68_face_landmarks.dat")
 
 (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
 (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
-cap=cv2.VideoCapture(0)
-flag=0
+
+cap = cv2.VideoCapture(0)
+flag = 0
+
+# Set your serial port and baudrate
+serial_port = "/dev/ttyACM0"  # Replace with your serial port
+serial_baudrate = 9600  # Replace with your baudrate
+
+# Start reading serial data in a separate thread
+serial_thread = threading.Thread(target=read_serial, args=(serial_port, serial_baudrate))
+serial_thread.start()
+
 while True:
-	ret, frame=cap.read()
-	frame = imutils.resize(frame, width=450)
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	subjects = detect(gray, 0)
-	for subject in subjects:
-		shape = predict(gray, subject)
-		shape = face_utils.shape_to_np(shape)#converting to NumPy Array
-		leftEye = shape[lStart:lEnd]
-		rightEye = shape[rStart:rEnd]
-		leftEAR = eye_aspect_ratio(leftEye)
-		rightEAR = eye_aspect_ratio(rightEye)
-		ear = (leftEAR + rightEAR) / 2.0
-		leftEyeHull = cv2.convexHull(leftEye)
-		rightEyeHull = cv2.convexHull(rightEye)
-		cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
-		cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
-		if ear < thresh:
-			flag += 1
-			print (flag)
-			if flag >= frame_check:
-				cv2.putText(frame, "****************ALERT!****************", (10, 30),
-					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-				cv2.putText(frame, "****************ALERT!****************", (10,325),
-					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-				#print ("Drowsy")
-		else:
-			flag = 0
-	cv2.imshow("Frame", frame)
-	key = cv2.waitKey(1) & 0xFF
-	if key == ord("q"):
-		break
+    ret, frame = cap.read()
+    frame = imutils.resize(frame, width=450)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    subjects = detect(gray, 0)
+
+    for subject in subjects:
+        shape = predict(gray, subject)
+        shape = face_utils.shape_to_np(shape)
+        leftEye = shape[lStart:lEnd]
+        rightEye = shape[rStart:rEnd]
+        leftEAR = eye_aspect_ratio(leftEye)
+        rightEAR = eye_aspect_ratio(rightEye)
+        ear = (leftEAR + rightEAR) / 2.0
+        leftEyeHull = cv2.convexHull(leftEye)
+        rightEyeHull = cv2.convexHull(rightEye)
+        cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+        cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+
+        if ear < thresh:
+            flag += 1
+            print(flag)
+            if flag >= frame_check:
+                cv2.putText(frame, "****************ALERT!****************", (10, 30),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                cv2.putText(frame, "****************ALERT!****************", (10, 325),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                # Send alert to Arduino (change this part as per your requirement)
+                serial_data = "ALERT\n"
+                ser.write(serial_data.encode())
+                print("Drowsy")
+
+        else:
+            flag = 0
+
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord("q"):
+        break
+
 cv2.destroyAllWindows()
-cap.release() 
+cap.release()
